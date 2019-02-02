0ed0d1c159526cd2a6b2ff9451bfd3db85a30008
jmattheis <contact@jmattheis.de>
2019-02-02 19:30:43 +0800

    add clearification on location

--- displayer.go
+++ displayer.go
@@ -6,6 +6,8 @@ import "net/url"
 // The text will appear on the plugin details page and can be multi-line.
 // Markdown syntax is allowed. Good for providing dynamically generated instructions to the user.
 // Location is the current location the user is accessing the API from, nil if not recoverable.
+// Location contains the path to the display api endpoint, you may only need the base url.
+// Example: https://gotify.net/plugin/5/display
 type Displayer interface {
 	Plugin
 	GetDisplay(location *url.URL) string

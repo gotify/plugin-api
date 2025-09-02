bdd4117d9d5579a6a673e5e0e22d007d60e8fed0
eternal-flame-AD <yume@yumechi.jp>
2025-09-02 11:37:22 -0500

    try to fix pipe_test on windows
    Signed-off-by: eternal-flame-AD <yume@yumechi.jp>

--- v2/transport/pipe_test.go
+++ v2/transport/pipe_test.go
@@ -1,37 +1,55 @@
 package transport
 
 import (
+	"log"
 	"net"
+	"net/url"
 	"testing"
 )
 
 func TestPipe(t *testing.T) {
-	listener, _, err := NewListener()
+	listener, addrURL, err := NewListener()
 	if err != nil {
-		panic(err)
+		t.Fatalf("failed to create listener: %v", err)
 	}
 	defer listener.Close()
 
+	urlParsed, err := url.Parse(addrURL)
+	if err != nil {
+		t.Fatalf("failed to parse address URL: %v", err)
+	}
+
+	var family string
+	if urlParsed.Scheme == "unix" {
+		family = "unix"
+	} else if urlParsed.Scheme == "dns" {
+		family = "tcp"
+	} else {
+		t.Fatalf("unsupported address URL scheme: %s", urlParsed.Scheme)
+	}
+
 	go func() {
-		conn, err := net.Dial("unix", listener.Addr().String())
+		conn, err := net.Dial(family, listener.Addr().String())
 		if err != nil {
-			panic(err)
+			log.Panicf("failed to dial listener: %v", err)
 		}
-		conn.Write([]byte("test"))
+		if _, err := conn.Write([]byte("test")); err != nil {
+			log.Panicf("failed to write to listener: %v", err)
+		}
 		conn.Close()
 	}()
 
 	accepted, err := listener.Accept()
 	if err != nil {
-		panic(err)
+		t.Fatalf("failed to accept listener: %v", err)
 	}
 	var buf [1024]byte
 	n, err := accepted.Read(buf[:])
 	if err != nil {
-		panic(err)
+		t.Fatalf("failed to read from listener: %v", err)
 	}
 	if string(buf[:n]) != "test" {
-		panic("expected test, got " + string(buf[:n]))
+		t.Fatalf("expected test, got %s", string(buf[:n]))
 	}
 	accepted.Write([]byte("test"))
 	accepted.Close()
